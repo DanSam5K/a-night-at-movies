c6fa08f96ed42d6ca16b4769fba0be791b0c5c89
Daniel Samuel <dansam5000@gmail.com>
2021-04-26 08:26:54 +0100

    prompt user for choice and also respond based choice with case statement

--- night_at_movies.rb
+++ night_at_movies.rb
@@ -1,6 +1,35 @@
 movies = {
-  matrix: 4.5,
-}
+  StarWars: 4.8, 
+  Divergent: 4.7
+  }
 
-puts "input your favorite movies: "
+puts "What would you like to do? "
+puts "Hints"
+puts "-- Type 'add' to add a movie."
+puts "-- Type 'update' to update a movie."
+puts "-- Type 'display' to display all movies."
+puts "-- Type 'delete' to delete a movie."
+
 choice = gets.chomp
+
+case choice
+when "add"
+  puts "Added!"
+when "update"
+  puts "Updated!"
+when "display"
+  puts "Movies!"
+when "delete"
+  puts "Deleted!"
+else
+  puts "Error!"
+end
+    
+    
+    
+    
+    
+    
+    
+    
+    
