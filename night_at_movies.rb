10026fd20eb2535f9af7fce43fb164a79aa99f54
Daniel Samuel <dansam5000@gmail.com>
2021-04-26 09:42:49 +0100

    fix rubocop linters errors

--- night_at_movies.rb
+++ night_at_movies.rb
@@ -1,11 +1,11 @@
 movies = {
-  StarWars: 4.8, 
+  StarWars: 4.8,
   Divergent: 4.7,
   Matrix: 5
 }
 
-puts "What would you like to do? "
-puts "Hints"
+puts 'What would you like to do? '
+puts 'Hints'
 puts "-- Type 'add' to add a movie."
 puts "-- Type 'update' to update a movie."
 puts "-- Type 'display' to display all movies."
@@ -14,50 +14,41 @@ puts "-- Type 'delete' to delete a movie."
 choice = gets.chomp
 
 case choice
-when "add"
-  puts "What movie would you like to add? "
+when 'add'
+  puts 'What movie would you like to add? '
   title = gets.chomp.to_sym
   if movies[title.to_sym].nil?
-    puts "What rating does the movie have? "
+    puts 'What rating does the movie have? '
     rating = gets.chomp.to_i
     movies[title] = rating
   else
     puts "That movie already exists! Its rating is #{movies[title.to_sym]}."
   end
 
-when "update"
-  puts "What movie would you like to update?"
+when 'update'
+  puts 'What movie would you like to update?'
   title = gets.chomp.to_sym
   if movies[title.to_sym].nil?
-    puts "movie is does not exist in library"
+    puts 'movie is does not exist in library'
   else
-    puts "What new rating does the movie have? "
+    puts 'What new rating does the movie have? '
     rating = gets.chomp
     movies[title.to_sym] = rating.to_i
-    puts "updated successfully"
+    puts 'updated successfully'
   end
-when "display"
-  movies.each do
-    |title, rating| puts "#{title}: #{rating}"
+when 'display'
+  movies.each do |movie_title, movie_rat|
+    puts "#{movie_title}: #{movie_rat}"
   end
-when "delete"
-  puts "what movie would you like to delete"
+when 'delete'
+  puts 'what movie would you like to delete'
   title = gets.chomp.intern
   if movies[title.intern].nil?
     puts "does not exist, can\'t delete"
   else
     movies.delete(title.intern)
-    puts "sucessfully deleted"
+    puts 'sucessfully deleted'
   end
 else
-  puts "Input a Valid choice see Hints!"
+  puts 'Input a Valid choice see Hints!'
 end
-    
-    
-    
-    
-    
-    
-    
-    
-    
